9784b0d944df66e7680bdc4b333d93523e7600e0
eedaymatlabcontest <eeedaymatlabcontest@gmail.com>
2015-12-09 18:13:03 +0600

    sdfsdf

--- beautifind.m
+++ beautifind.m
@@ -4,7 +4,7 @@ function [ k,s] = beautifind( n,m )
 % Beautiful Triangle
 %
 % [k,s]= beautyfind(n,m)
-% If n=2, m=2, output should be k=1, s= [1,2]
+% If n=2, m=2, output should be k=3, s= [1,1;1,2;2,2]
 
 k = [];
 s = [];
